ab4b38d6284c2c44386dafc251b688fcf3472922
Kaitong <379653872@qq.com>
2018-05-08 14:00:11 +0800

    creat

---
+++ ajax/get.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<?php
+$a = $_GET['user'];
+$b = $_GET['pwd'];
+echo '我是一个php 页面 接受到 {$a} , {$b}'
+?>
---
+++ ajax/post.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<?php
+$a = $_POST['user'];
+$b = $_POST['pwd'];
+echo '我是一个php 页面 接受到 {$a} , {$b}'
+?>
